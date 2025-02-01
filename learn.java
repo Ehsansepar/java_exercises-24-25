02836881d7cdd72d5807883241a68b476236d163
Ehsan SEPAR <100959074+Ehsansepar@users.noreply.github.com>
2025-02-01 14:34:01 +0100

    Add email validation and username extraction exercise

--- learn.java
+++ learn.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class learn {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name = "";
+        // Scanner scanner = new Scanner(System.in);
+        // String name = "";
         
-        System.out.print("Enter your name: ");
-        name = scanner.nextLine();
+        // System.out.print("Enter your name: ");
+        // name = scanner.nextLine();
         
-        while (name.contains(" ")) {
-            System.out.println("Please enter your name without any spaces");
-            System.out.print("Enter your name: ");
-            name = scanner.nextLine();
-        }
+        // while (name.contains(" ")) {
+        //     System.out.println("Please enter your name without any spaces");
+        //     System.out.print("Enter your name: ");
+        //     name = scanner.nextLine();
+        // }
 
-        System.out.println("Hello, " + name + "!");
+        // System.out.println("Hello, " + name + "!");
         
-        scanner.close();
+        // scanner.close();
 
         // learnning
 
@@ -34,6 +34,32 @@ public class learn {
         // String removeSpace = nameWithSpace.trim();
         // String replaceString = name.replace(nameWithSpace, removeSpace);
 
+//  ---------------------------------------------------------------------------------------
+                // Creation Account Test 
+        // Scanner scanner = new Scanner(System.in);
 
+        // System.out.print("Email : ");
+        // String email = scanner.nextLine();
+        
+        // // Vérifie si l'email contient à la fois @ ET .com
+        // while(!email.contains("@") || !email.contains(".com") || email.indexOf("@") > email.indexOf(".com")) {
+        //     System.out.println("Enter a valid Email please!");  
+        //     System.out.print("Email : ");
+        //     email = scanner.nextLine();      
+        // }
+
+        // System.out.print("Password : ");
+        // String password = scanner.nextLine();
+
+        // // Extraire le nom d'utilisateur (tout ce qui est avant @)
+        // String username = email.substring(0, email.indexOf("@"));
+        // String emailDomain = email.substring(email.indexOf("@") + 1);
+        
+        // System.out.printf("Hello %s !\nYour Demain : %s", username, emailDomain);
+
+        // scanner.close();
+
+// -------------------------------------------------------------------------------------------------------------------
+        // Another Exercice will come ...
     }
 }
