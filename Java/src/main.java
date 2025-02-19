a2bd50c2e684d8a37ab33faff3188cffc78829c0
Ehsansepar <ehsan.separ007@gmail.com>
2025-02-19 14:01:11 +0100

    all classes tested and its working fine

---
+++ Java/src/main.java
@@ -0,0 +1,15 @@
+import array.exe03;
+public class main {
+    public static void main(String[] args) {
+        exe03 trouver = new exe03();
+        int[] nombre = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        int result = trouver.TrouverLaMaximumListe(nombre);
+
+        System.out.println(result);
+
+    }
+}
+
+// testing the classes 
+
