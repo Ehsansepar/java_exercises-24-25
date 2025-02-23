d0e8bd77b4be7b29158974538829a4fe7740ef30
Ehsansepar <ehsan.separ007@gmail.com>
2025-02-23 22:17:46 +0100

    Comment out unused variables and code in the main method

--- test.java
+++ test.java
@@ -4,25 +4,25 @@ public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int row;
-        int columns;
-        char symbol;
+        // int row;
+        // int columns;
+        // char symbol;
 
-        System.out.print("Enter the # of the row : ");
-        row = scanner.nextInt();
+        // System.out.print("Enter the # of the row : ");
+        // row = scanner.nextInt();
 
-        System.out.print("Enter the # of the columns : ");
-        columns = scanner.nextInt();
+        // System.out.print("Enter the # of the columns : ");
+        // columns = scanner.nextInt();
 
-        System.out.print("Enter the symbol : ");
-        symbol = scanner.next().charAt(0);
+        // System.out.print("Enter the symbol : ");
+        // symbol = scanner.next().charAt(0);
         
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(symbol + " ");
-            }
-            System.out.println();
-        }
+        // for (int i = 0; i < row; i++) {
+        //     for (int j = 0; j < columns; j++) {
+        //         System.out.print(symbol + " ");
+        //     }
+        //     System.out.println();
+        // }
 
 
 
