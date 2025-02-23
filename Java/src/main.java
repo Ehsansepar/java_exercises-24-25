99db8e12c3814d0c9becefab6b0137e48208eaf9
Ehsansepar <ehsan.separ007@gmail.com>
2025-02-23 22:18:03 +0100

    Update main method to use exe13 class for finding maximum and minimum values in an array

--- Java/src/main.java
+++ Java/src/main.java
@@ -1,12 +1,14 @@
-import array.exe03;
+import array.exe13;
 public class main {
     public static void main(String[] args) {
-        exe03 trouver = new exe03();
+        exe13 trouver = new exe13(); // testing connection with class
         int[] nombre = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-        int result = trouver.TrouverLaMaximumListe(nombre);
+        int max = exe13.MaxInsideList(nombre);
+        int min = exe13.MinInsideList(nombre);
 
-        System.out.println(result);
+        System.out.println(max);
+        System.out.println(min);
 
     }
 }
