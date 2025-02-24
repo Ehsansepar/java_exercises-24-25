114725bf5e44296ed3a12612b6eb2fdf18f30b85
Ehsansepar <ehsan.separ007@gmail.com>
2025-02-24 23:16:54 +0100

    Refactor book management: add afficherBooks method and streamline addBook method

--- small_project/library_v01.java
+++ small_project/library_v01.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class library_v01 {
+    public static ArrayList<String> books = new ArrayList<String>();
+
+    public static String afficherBooks(ArrayList<String> books) {
+        for (int i=0; i<books.size();i++) {
+            System.out.println(i + ". " + books.get(i));
+        }
+        return books.toString();
+    }
 
     public static void menu() {
         System.out.println("1. Afficher la liste des livres");
@@ -11,44 +19,35 @@ class library_v01 {
         System.out.println("4. Modifier un livre");
         System.out.println("5. Quitter");
         System.out.println();
-
-        
     }
 
-    public static void addBook(ArrayList<String> books) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Entrez le titre du livre : ");
+    public static void addBook(Scanner scanner) {
+        System.out.print("Entrez le titre du livre : ");
         String title = scanner.nextLine();
         books.add(title);
 
-        System.out.println("Entrez le nom de l'auteur : ");
+        System.out.print("Entrez le nom de l'auteur : ");
         String author = scanner.nextLine();
         books.add(author);
-
-        scanner.close();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<String> books = new ArrayList<String>();
-        // books.add("The Catcher in the Rye");
-        // books.add("To Kill a Mockingbird");
-        // books.add("1984");
 
         int choice = 0;
         do {
             menu();
             System.out.print("-> : ");
             choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+            System.out.println();
 
             switch (choice) {
                 case 1:
-                    System.out.println(books);
+                    System.out.println(afficherBooks(books));
                     break;
                 case 2:
-                    addBook(books);
+                    addBook(scanner);
                     break;
                 case 3:
                     // deleteBook(books);
