e290eea2ab65cdc0a2ea039bb23fe06d70f0b121
Ehsan SEPAR <100959074+Ehsansepar@users.noreply.github.com>
2025-02-26 23:16:10 +0100

    GitHub connection lost

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
@@ -29,3 +29,5 @@ public class test {
         scanner.close();
     }   
 }
+
+// testing 
